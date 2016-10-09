41a40b38cd166ce4e5ca4f0a0dc9a2269a70c243
zzgo <531818100@qq.com>
2016-10-09 10:31:38 +0800

    +初始化，短信验证Demo

---
+++ MobSMSDemo/MobSMSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobSMSDemo
+//
+//  Created by zzgo on 16/10/9.
+//  Copyright © 2016年 zzgo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobSMSDemo/MobSMSDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobSMSDemo
+//
+//  Created by zzgo on 16/10/9.
+//  Copyright © 2016年 zzgo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
