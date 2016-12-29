0819e6541e89ef54fa8dd0e6403a52c0d508802e
zzgo <zzgocc@gmail.com>
2016-12-29 22:35:19 +0800

    +aopdemo

---
+++ AdCellDemo/AdCellDemo/AdCellDemo/HZAdAOP.h
@@ -0,0 +1,14 @@
+//
+//  HZAdAOP.h
+//  gosparkchat
+//
+//  Created by ZernonHuang on 2016/12/27.
+//  Copyright © 2016年 gospark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <IMYAOPTableView.h>
+
+@interface HZAdAOP : NSObject
+@property (weak, nonatomic) IMYAOPTableViewUtils *aopUtils;
+@end
--- AdCellDemo/AdCellDemo/AdCellDemo/ViewController.m
+++ AdCellDemo/AdCellDemo/AdCellDemo/ViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "HZAdAOP.h"
 
-@interface ViewController ()
+static NSString *const cellID=@"tableViewCellID";
 
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic,readwrite,strong) UITableView *tableView;
+@property (nonatomic,readwrite,strong) HZAdAOP     *aopDemo;
 @end
 
 @implementation ViewController
@@ -17,6 +21,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.tableView];
+    //begin 插入代码
+    self.aopDemo = [HZAdAOP new];
+    self.aopDemo.aopUtils = self.tableView.aop_utils;
+    //end
 }
 
 
@@ -25,5 +34,29 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark -  UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
 
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:cellID
+                                                          forIndexPath:indexPath];
+    cell.textLabel.text=[NSString stringWithFormat:@"%zd",indexPath.row];
+    return cell;
+}
+#pragma mark - getter
+-(UITableView *)tableView{
+    if (!_tableView) {
+        _tableView=[[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
+        [_tableView registerClass:[UITableViewCell class] 
+           forCellReuseIdentifier:cellID];
+        _tableView.delegate=self;
+        _tableView.dataSource=self;
+    }
+    return _tableView;
+}
 @end
