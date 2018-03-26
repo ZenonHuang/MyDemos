de4f9f7dca39f250a8641ec8d1a4988fc914743c
zenon <zzgocc@gmail.com>
2018-03-26 10:09:10 +0800

    +KVOTest

---
+++ KVOTest/KVOTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVOTest
+//
+//  Created by mewe on 2018/3/23.
+//  Copyright © 2018年 zenon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVOTest/KVOTest/Fizz.h
@@ -0,0 +1,17 @@
+//
+//  Fizz.h
+//  KVOTest
+//
+//  Created by mewe on 2018/3/23.
+//  Copyright © 2018年 zenon. All rights reserved.
+//
+
+
+
+#import <Foundation/Foundation.h>
+
+@interface Fizz : NSObject
+
+@property (nonatomic, strong) NSNumber *number;
+
+@end
