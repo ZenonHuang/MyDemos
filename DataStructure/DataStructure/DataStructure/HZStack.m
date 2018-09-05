fbd0636ea2a5fedbf87851e082d05a2f5498437c
zenon <zzgocc@gmail.com>
2018-09-05 23:21:45 +0800

    DataStructure Init

---
+++ DataStructure/DataStructure/DataStructure/HZStack.m
@@ -0,0 +1,18 @@
+//
+//  HZStack.m
+//  DataStructure
+//
+//  Created by zz go on 2018/3/13.
+//  Copyright © 2018年 zenonhuang. All rights reserved.
+//
+
+#import "HZStack.h"
+
+@interface HZStack ()
+
+@end
+
+@implementation HZStack
+
+
+@end
---
+++ DataStructure/DataStructure/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DataStructure
+//
+//  Created by zz go on 2018/3/13.
+//  Copyright © 2018年 zenonhuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
